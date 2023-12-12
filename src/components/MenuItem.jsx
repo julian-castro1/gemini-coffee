47fe08119027f610d29e978d84c42bc241b61ef9
Julian Castro <julian_castro1@icloud.com>
2023-12-11 20:46:49 -0600

    created menu items

---
+++ src/components/MenuItem.jsx
@@ -0,0 +1,72 @@
+import styled from "styled-components";
+
+function MenuItem({name, price, description, img}){
+    function formatCurrency(num) {
+        return new Intl.NumberFormat('en-US', {
+            style: 'currency',
+            currency: 'USD',
+            minimumFractionDigits: 2
+        }).format(num);
+    }
+
+    return(
+        <MenuItemContainer>
+            <MenuItemImg src={img}/>
+            <DescContainer>
+                <MenuItemName>{name}</MenuItemName>
+                <MenuItemDescription>{description}</MenuItemDescription>
+            </DescContainer>
+            <MenuItemPrice><span>{formatCurrency(price)}</span></MenuItemPrice>
+        </MenuItemContainer>
+    )
+}
+
+const MenuItemContainer = styled.div`
+    display: flex;
+    width: 90%;
+
+    border-radius: 1rem;
+
+    color: ${props=>props.theme.color};
+
+    background-color: ${props=>props.theme.background};
+`;
+const MenuItemImg = styled.img`
+    flex: 1;
+    height: 6rem;
+
+    border-radius: 1rem 0 0 1rem;
+
+    background-color: ${props=>props.theme.color};
+`
+const DescContainer = styled.div`
+    flex: 2;
+    display: flex;
+    flex-direction: column;
+    justify-content: center;
+    align-items: center;
+`
+const MenuItemName = styled.div`
+    font-weight: 700;
+    font-size: 1.3rem;
+`
+const MenuItemDescription = styled.div`
+    font-weight: 200;
+    font-size: .8rem;
+`
+const MenuItemPrice = styled.div`
+    flex: .2;
+    display: flex;
+    justify-content: right;
+
+    font-size: .8rem;
+
+    margin-top: auto;
+    padding-bottom: .75rem;
+    padding-right: .75rem;
+`
+
+
+
+
+export default MenuItem;
--- src/components/MenuSection.jsx
+++ src/components/MenuSection.jsx
@@ -1,11 +1,58 @@
 import styled from "styled-components";
 import Title from "./Title";
 import SubTitle from "./SubTitle";
+import MenuItem from "./MenuItem";
 
 function MenuSection(){
 
     const syrups = 'add syrup (caramel, vanilla, mocha) for $0.50';
     const milks = 'available with whole, 2%, almond, and oat milk';
+    const disclaimer = 'subject to availability'
+
+    const menuOptions = [
+        {
+            name: 'cappuccino',
+            type: 'drink',
+            price: 5.50,
+            description: 'double shot',
+            img: '',
+        },
+        {
+            name: 'latte',
+            type: 'drink',
+            price: 6.00,
+            description: 'double shot',
+            img: '',
+        },
+        {
+            name: 'americano',
+            type: 'drink',
+            price: 4.50,
+            description: 'double shot',
+            img: '',
+        },
+        {
+            name: 'greek salad',
+            type: 'food',
+            price: 15.00,
+            description: 'double shot',
+            img: '',
+        },
+        {
+            name: 'bruschetta',
+            type: 'food',
+            price: 12.00,
+            description: 'double shot',
+            img: '',
+        },
+        {
+            name: 'avocado toast',
+            type: 'food',
+            price: 7.50,
+            description: 'double shot',
+            img: '',
+        },
+    ]
 
     return(
         <MenuContainer>
@@ -13,9 +60,40 @@ function MenuSection(){
                 <Title text='coffee'/>
                 <SubTitle text={syrups}/>
                 <SubTitle text={milks}/>
+                <DrinkItems>
+                    {menuOptions.map((item, index)=>{
+                        if(item.type==='drink'){
+                            return(
+                                <MenuItem 
+                                    key={index}
+                                    name={item.name}
+                                    price={item.price}
+                                    description={item.description}
+                                    img={item.img}
+                                />
+                            )
+                        }
+                    })}
+                </DrinkItems>
             </CoffeeSection>
             <BitesSection>
-                
+                <Title text='bites'/>
+                <SubTitle text={disclaimer}/>
+                <DrinkItems>
+                    {menuOptions.map((item, index)=>{
+                        if(item.type==='food'){
+                            return(
+                                <MenuItem 
+                                    key={index}
+                                    name={item.name}
+                                    price={item.price}
+                                    description={item.description}
+                                    img={item.img}
+                                />
+                            )
+                        }
+                    })}
+                </DrinkItems>
             </BitesSection>
         </MenuContainer>
     )
@@ -28,17 +106,31 @@ const MenuContainer = styled.div`
 
     background-color: ${props=>props.theme.back2};
 
-    height: 70vh;
+    /* height: 70vh; */
 `
 const CoffeeSection = styled.div`
     display: flex;
     flex-direction: column;
     align-items: center;
 
-    height: 3rem;
+    width: 100%;
+`
+const DrinkItems = styled.div`
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+    margin-top: 1.5rem;
+
+    width: 100%;
+
+    gap: .5rem;
 `
 const BitesSection = styled.div`
+    display: flex;
+    flex-direction: column;
+    align-items: center;
 
+    width: 100%;
 `
 
 export default MenuSection;
