caacec6e30931ef4de306e0c6266b2b70d819b8b
Julian Castro <julian_castro1@icloud.com>
2023-12-11 21:46:57 -0600

    checking image

--- src/components/StorySection.jsx
+++ src/components/StorySection.jsx
@@ -1,11 +1,13 @@
 import styled from "styled-components";
 import Title from "./Title";
+import usImg from "../assets/couple.png";
 
 function StorySection(){
 
     return(
         <StoryContainer>
             <Title text='our story'/>
+            <ImageContainer src={usImg}/>
         </StoryContainer>
     )
 }
@@ -18,6 +20,17 @@ const StoryContainer = styled.div`
     padding-bottom: 1.5rem;
 
     background-color: ${props=>props.theme.back2};
+`
+const ImageContainer = styled.img`
+    width: 90%;
+    /* height: 20rem; */
+
+    border-radius: 1rem;
+
+    padding-bottom: 0;
+    margin-bottom: 0;
+
+    background-color: ${props=>props.theme.color};
 `
 
 export default StorySection;
