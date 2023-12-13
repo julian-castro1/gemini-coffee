9631aa449459a26011e6dde71cf81def659126db
Julian Castro <julian_castro1@icloud.com>
2023-12-13 02:09:56 -0600

    connected info

--- src/components/ConnectedSection.jsx
+++ src/components/ConnectedSection.jsx
@@ -3,15 +3,34 @@ import Title from "./Title";
 
 function ConnectedSection({id}){
 
+    const text = 'Connect with us on social media to keep up with new releases and location changes!'; 
+    const IGhandle = '@geminicoffee.co'
+
     return(
         <ConnectedContainer id={id}>
             <Title text='stay connected'/>
+            <Body>{text}</Body>
+            <Body>{IGhandle}</Body>
         </ConnectedContainer>
     )
 }
 
 const ConnectedContainer = styled.div`
+    display: flex;
+    flex-direction: column;
+    gap: .5rem;
+
+    padding: 0 1.5rem 2.25rem 1.5rem;
+`
+const Body = styled.span`
+    padding-left: .7rem;
+    padding-right: .7rem;
+    padding-top: ${props=>props.signoff ? '1.5rem' : '0'};
+    font-size: .8rem;
+
     
+    color: ${props=>props.theme.color};
+
 `
 
 export default ConnectedSection;
